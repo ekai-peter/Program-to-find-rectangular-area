de39d29d168fba1eb0757655f4aab663520fb9ec
ekai-peter <ekaipeter58@gmail.com>
2025-02-21 03:22:10 -0600

    Add files via upload

---
+++ area.c
@@ -0,0 +1,21 @@
+/*
+author:ekaipeter
+reg no.d33-2442-2023
+Program:to find area of rectangle 
+date:20/2/2025
+*/
+#include<stdio.h>//printf()
+int main(){
+// variable decelerations and initialization;
+int l;
+int w;
+int area;
+printf("program to multiply 2 numbers: \n");
+    printf("enter the first number:\n");
+    scanf("%d",&l);
+    printf("enter the second number:\n");
+    scanf("%d",&w);
+    area=l*w;
+    printf ("the area is %d",area);
+    return 0;
+}
